4be9f3a2aa0780d9d1b8bde96d313f88ba27aff4
Jacson R. Sasse <jacson.sasse@hotmail.com>
2022-10-15 20:19:00 -0300

    Tested autowiring in lower classes

---
+++ src/tests/Entity/TestEntity.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Jrs\Tests\Entity;
+
+class TestEntity
+{
+}
--- src/tests/Service/SecondTestService.php
+++ src/tests/Service/SecondTestService.php
@@ -2,8 +2,15 @@
 
 namespace Jrs\Tests\Service;
 
+use Jrs\Tests\Entity\TestEntity;
+
 class SecondTestService implements ServiceInterface
 {
+    public function __construct(private TestEntity $entity)
+    {
+        echo 'Injetou a classe automaticamente!';
+    }
+
     public function execute(): mixed
     {
         echo 'Deu boa 2';
