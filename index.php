81b878f1438d4ccfa773ca32baeb04ea9a8f2473
Jacson R. Sasse <jacson.sasse@hotmail.com>
2022-10-30 18:10:10 -0300

    Just some adjusts, removing classes that no longer exist

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 
 use Jrs\DI\Container;
-use Jrs\Tests\Controller\TestController;
-use Jrs\Tests\Service\SecondTestService;
-use Jrs\Tests\Service\ServiceInterface;
-use Jrs\Tests\Service\TestService;
+use Jrs\Example\SimpleClass;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $container = new Container();
-$container->get(TestController::class, [ServiceInterface::class => ['class' => TestService::class]])->index();
-echo '<br />';
-$container->get(TestController::class, [ServiceInterface::class => ['class' => SecondTestService::class]])->index();
+$simpleClass = $container->get(SimpleClass::class);
+echo $simpleClass->print();
